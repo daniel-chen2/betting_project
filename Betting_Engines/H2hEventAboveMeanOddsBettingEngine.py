be44c4ca3333358440910d219a7cc42c46375de7
Daniel <d.chen@live.com>
2025-07-31 20:07:58 +0800

    save

--- Betting_Engines/H2hEventAboveMeanOddsBettingEngine.py
+++ Betting_Engines/H2hEventAboveMeanOddsBettingEngine.py
@@ -17,28 +17,31 @@ class H2hEventAboveMeanOddsBettingEngine:
         worthyBets = []
         for index, bookmaker in bookmakers_df.iterrows():
             for outcome in ["home_team", "away_team", "draw"]:
-                if(bookmaker[f"{outcome}_back_odds"] is None):
+                if(bookmaker[f"{outcome}_back_odds"] is None or pd.isna(bookmaker[f"{outcome}_back_odds"])):
                     break
                 probabilityMedian = 1/bookmakers_df[f"{outcome}_back_odds"].median(numeric_only=True)
                 if(self.__willBet(bookmaker[f"{outcome}_back_odds"], probabilityMedian, self.alpha, self.commision, self.betOddsUpperLimit)):
                     worthyBets.append({"bookmaker": bookmaker["title"], "guessed_outcome": H2hOutcome(outcome), "odds": bookmaker[f"{outcome}_back_odds"], "median": 1/probabilityMedian})
+                    # Remove later just to test
+                    break
+                    
         return worthyBets
 
     def analyseAndFindBets(self, h2hEvents: list[H2hEvent]) -> list[H2hBet]: 
         events_df = pd.DataFrame(h2hEvents).sort_values(by='commence_time', ascending=True)
+        betsFound = []
         for index, event in events_df.iterrows():
             bookmakers_df = pd.DataFrame(event["bookmakers"])
             if(len(bookmakers_df) == 0):
                 continue
-
-            print(self.__getWorthyBets(bookmakers_df))
             worthyBetsFound = self.__getWorthyBets(bookmakers_df)
             if(len(worthyBetsFound) == 0):
-                print("no bets found")
-                return
+                continue
 
-            betsFound = []
             for betFound in worthyBetsFound:
+                b = (betFound["odds"] - 1)
+                p = (1/betFound["median"])
+                kellyBetAmount = ((b * p) - (1-p))/b * self.betAmount
                 betsFound.append(
                     H2hBet(
                         id= event.id,
@@ -47,11 +50,11 @@ class H2hEventAboveMeanOddsBettingEngine:
                         away_team=event.away_team,
                         commence_time=event.commence_time,
                         bookmaker=betFound["bookmaker"],
-                        betAmount=self.betAmount,
+                        betAmount=kellyBetAmount,
                         guessed_outcome=betFound["guessed_outcome"],
                         odds=betFound["odds"],
                         outcome=event.outcome,
-                        extra_notes=f"median = {betFound["median"]}"
+                        extra_notes=f"median = {betFound["median"]} b = {b}, p = {p}"
                     )
                 )
-            return betsFound
+        return betsFound
